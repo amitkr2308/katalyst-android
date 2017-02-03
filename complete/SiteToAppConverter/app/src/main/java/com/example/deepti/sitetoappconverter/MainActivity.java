af535fe1c6185073dbdccb8b49d641185b37df1e
Rajesh Gheware <rajeshgheware@gmail.com>
2017-02-03 13:47:06 +0530

    first android project

---
+++ complete/SiteToAppConverter/app/src/main/java/com/example/deepti/sitetoappconverter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.deepti.sitetoappconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://katalystindia.org");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(myWebView.canGoBack()){
+            myWebView.goBack();
+        }else{
+            super.onBackPressed();
+        }
+    }
+}
